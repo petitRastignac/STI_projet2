33ec9e543ecb7985cc249242049d06574c48c8a7
Serphentas <Serphentas@users.noreply.github.com>
2020-10-05 18:22:02 +0200

    fix sqlalchemy config errors

--- messenger/config.py
+++ messenger/config.py
@@ -4,3 +4,4 @@ from messenger.security import gen_rand_string
 class Config:
     SECRET_KEY = gen_rand_string()
     SQLALCHEMY_DATABASE_URI = os.environ.get('STI_MSN_DB') or 'sqlite:///db.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
