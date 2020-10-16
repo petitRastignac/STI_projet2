c6dc6ecf4f46a53a9d28c29cd83baac210c74868
Serphentas <Serphentas@users.noreply.github.com>
2020-10-16 23:54:46 +0200

    no scrypt

--- messenger/security.py
+++ messenger/security.py
@@ -4,24 +4,22 @@
 #But :
 #Remarque :
 #------------------------------
-import random, string, base64
+import random, string, base64, hmac
 
-from flask_scrypt import generate_random_salt, generate_password_hash, check_password_hash, debase64, enbase64
 
 SCRYPT_N = 65536
 SCRYPT_SALT_LEN = 16
 API_ID_LEN = 24
 
-def implode(pw_hash, salt, N):
+def implode(pw_hash, salt):
     """
     Implodes the given hash and its arguments into a string
 
     :param pw_hash: password hash
-    :param salt: scrypt salt
-    :param N: scrypt N factor
-    :returns: hash string with arguments in order (N,salt,hash)
+    :param salt: hash salt
+    :returns: hash string with arguments in order (salt,hash)
     """
-    return '$' + str(N) + '$' + salt + '$' + pw_hash
+    return '$' + salt + '$' + pw_hash
 
 def explode(hashstr):
     """
@@ -31,11 +29,7 @@ def explode(hashstr):
     :returns: elements of the given hash string as an array
     """
 
-    indexes = [pos for pos, char in enumerate(hashstr) if char == '$']
-    N = hashstr[indexes[0]+1:indexes[1]]
-    salt = hashstr[indexes[1]+1:indexes[2]]
-    pw_hash = hashstr[indexes[2]+1:]
-    return [pw_hash, salt, N]
+    return hashstr.split('$')
 
 def hash_pw(password):
     """
@@ -45,9 +39,16 @@ def hash_pw(password):
     :returns: hashed password with arguments
     """
 
-    salt = generate_random_salt(SCRYPT_SALT_LEN)
-    pw_hash = generate_password_hash(password, salt, N=SCRYPT_N)
-    return implode(pw_hash.decode('ASCII'), salt.decode('ASCII'), SCRYPT_N)
+    salt = gen_rand_string()
+    pw_hash = base64.b64encode(
+        hmac.new(
+            key=salt.encode('utf8'),
+            msg=password.encode('utf8'),
+            digestmod='SHA256'
+        ).digest()
+    ).decode('utf8')
+
+    return implode(pw_hash, salt)
 
 def check_pw(password, pw_hash):
     """
@@ -59,12 +60,13 @@ def check_pw(password, pw_hash):
     """
 
     hashvars = explode(pw_hash)
-    return check_password_hash(
-        str(password),
-        str.encode(hashvars[0]),
-        str.encode(hashvars[1]),
-        int(hashvars[2])
-    )
+    mac = base64.b64decode(pw_hash)
+
+    return hmac.compare_digest(hashvars[2].encode('utf8'), base64.b64encode(hmac.new(
+        key=hashvars[1].encode('utf8'),
+        msg=password.encode('utf8'),
+        digestmod='SHA256'
+    ).digest()))
 
 def gen_rand_string(prefix=None):
     """
